67d547f08ff08d3b250de34ac4b7a46719e6e6de
v_jpch <v_jpch.digitalgd.com>
2025-01-06 15:20:08 +0800

    feat: 实现拖拽左侧组件渲染到中间容器内

--- src/packages/editor-block.jsx
+++ src/packages/editor-block.jsx
@@ -1,4 +1,4 @@
-import { computed, defineComponent, inject } from "vue";
+import { ref, computed, defineComponent, inject, onMounted } from "vue";
 
 export default defineComponent({
   props: {
@@ -18,13 +18,28 @@ export default defineComponent({
     // 读取组件config配置
     const config = inject("config");
 
+    const blockRef = ref(null);
+    // 渲染元素后，拿到元素的宽高，处理居中
+    onMounted(() => {
+      const { offsetWidth, offsetHeight } = blockRef.value;
+      // 拖拽松手的时候需要居中
+      if (props.block.alignCenter) {
+        // 处理元素居中
+        props.block.left = props.block.left - offsetWidth / 2;
+        props.block.top = props.block.top - offsetHeight / 2;
+
+        // 重置状态
+        props.block.alignCenter = false;
+      }
+    });
+
     return () => {
       // 找到组件
       const component = config.componentMap[props.block.key];
       // 拿到要渲染的真实组件
       const RenderComponent = component.render();
       return (
-        <div className="editor-block" style={blockStyles.value}>
+        <div className="editor-block" style={blockStyles.value} ref={blockRef}>
           {/* 这里不能使用标签形式来渲染，如<RenderComponent /> */}
           {RenderComponent}
         </div>
--- src/packages/editor.jsx
+++ src/packages/editor.jsx
@@ -1,4 +1,5 @@
-import { computed, defineComponent, inject } from "vue";
+import { ref, computed, defineComponent, inject } from "vue";
+import deepcopy from "deepcopy";
 
 // 引入样式
 import "./editor.scss";
@@ -10,12 +11,15 @@ export default defineComponent({
   props: {
     modelValue: { type: Object, required: true },
   },
-  setup(props) {
-    console.log(props.data);
-
+  emits: ["update:modelValue"], // 要触发的事件
+  setup(props, ctx) {
     const data = computed({
       get() {
         return props.modelValue;
+      },
+      set(newVal) {
+        // 将数据进行深拷贝后派发出去
+        ctx.emit("update:modelValue", deepcopy(newVal));
       },
     });
 
@@ -28,13 +32,76 @@ export default defineComponent({
     // 注入组件配置
     const config = inject("config");
 
+    // 容器ref
+    const containerRef = ref();
+
+    // 当前拖拽组件是谁
+    const currentComponent = ref(null);
+
+    const dragenter = (e) => {
+      e.dataTransfer.dropEffect = "move";
+    };
+    const dragover = (e) => {
+      e.preventDefault();
+    };
+    const dragleave = (e) => {
+      e.dataTransfer.dropEffect = "none";
+    };
+    const drop = (e) => {
+      let blocks = data.value.blocks; // 拿到内部已经渲染的组件
+      // 更新双向绑定的值
+      data.value = {
+        ...data.value,
+        blocks: [
+          ...blocks,
+          {
+            top: e.offsetY,
+            left: e.offsetX,
+            key: currentComponent.value.key,
+            zIndex: 1,
+            alignCenter: true, // 希望松手的时候渲染元素居中
+          },
+        ],
+      };
+      // 用完后置空当前组件
+      currentComponent.value = null;
+    };
+
+    // 拖拽左侧组件时，给容器绑定各种事件
+    const dragstart = (e, component) => {
+      // dragenter 进入目标元素 添加移动标识
+      // dragover 经过目标元素（必须要阻止默认行为，否则无法触发drop）
+      // dragleave 离开目标元素 增加禁用标识
+      // drop 松手的时候 根据拖拽组件添加该组件到目标元素
+      containerRef.value.addEventListener("dragenter", dragenter);
+      containerRef.value.addEventListener("dragover", dragover);
+      containerRef.value.addEventListener("dragleave", dragleave);
+      containerRef.value.addEventListener("drop", drop);
+
+      // 记录当前拖拽的组件是谁
+      currentComponent.value = component;
+    };
+
+    // 拖拽左侧组件结束后移除容器的各个事件，释放内存
+    const dragend = (e) => {
+      containerRef.value.removeEventListener("dragenter", dragenter);
+      containerRef.value.removeEventListener("dragover", dragover);
+      containerRef.value.removeEventListener("dragleave", dragleave);
+      containerRef.value.removeEventListener("drop", drop);
+    };
+
     return () => (
       <div className="editor">
         {/* 负责左侧预览组件 */}
         <div className="editor-left">
-          {/* 根据注册列表渲染预览内容 */}
+          {/* 根据注册列表渲染预览内容，实现h5的拖拽 */}
           {config.componentList.map((component) => (
-            <div className="editor-left-item">
+            <div
+              className="editor-left-item"
+              draggable
+              onDragstart={(e) => dragstart(e, component)}
+              onDragend={dragend}
+            >
               <span>{component.label}</span>
               <div>{component.preview()}</div>
             </div>
@@ -50,6 +117,7 @@ export default defineComponent({
             <div
               className="editor-container-canvas__content"
               style={containerStyles.value}
+              ref={containerRef}
             >
               {/* 动态渲染所有内容块 */}
               {data.value.blocks.map((block) => (
